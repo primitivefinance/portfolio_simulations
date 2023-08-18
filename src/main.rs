fb2c23fd8e736dfcdf3d0834890a6a8f89482438
Colin Roberts <colin@primitive.xyz>
2023-08-18 08:31:39 -0600

    contract deployment and logging

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,18 @@ use arbiter_core::manager::Manager;
 use arbiter_core::middleware::RevmMiddleware;
 use ethers::providers::Middleware;
 use std::sync::Arc;
+use log::info;
 
 mod bindings;
 mod startup;
 
+pub type Client = Arc<RevmMiddleware>;
+
 // Environment
 const ENV_LABEL: &str = "portfolio";
+
+// Admin
+const ADMIN_LABEL: &str = "admin";
 
 // Tokens
 const ARBITER_TOKEN_X_NAME: &str = "Arbiter Token X";
@@ -20,21 +26,23 @@ const ARBITER_TOKEN_Y_NAME: &str = "Arbiter Token Y";
 const ARBITER_TOKEN_Y_SYMBOL: &str = "Arbiter Token Y";
 const ARBITER_TOKEN_Y_DECIMALS: u8 = 18;
 
+const BLOCK_RATE: f64 = 10.0;
+const BLOCK_SEED: u64 = 0;
+
+const INITIAL_PRICE: u64 = 1000;
+
 #[tokio::main]
 pub async fn main() -> Result<()> {
-    let mut manager = Manager::new();
-    manager.add_environment(ENV_LABEL, 1.0, 1)?;
+        // Initialize the logger
+        if std::env::var("RUST_LOG").is_err() {
+            std::env::set_var("RUST_LOG", "debug");
+        }
+        env_logger::init();
 
-    let client = Arc::new(RevmMiddleware::new(
-        manager.environments.get(ENV_LABEL).unwrap(),
-        Some("client".to_string()),
-    ));
-    println!(
-        "created client with address {:?}",
-        client.default_sender().unwrap()
-    );
-    manager.start_environment(ENV_LABEL)?;
-    startup::deploy_initial_contracts(client.clone()).await?;
+
+    let (_manager, admin) = startup::initialize()?;
+    let (_weth, _arbx, _arby, _liquid_exchange, _portfolio) =
+        startup::deploy_contracts(admin.clone()).await?;
 
     Ok(())
 }
