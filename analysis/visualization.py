05de20be30cfcd49e72b9c7b71cbf8c1f483da66
Colin Roberts <colin@primitive.xyz>
2023-08-25 10:22:28 -0600

    refactor the plotter

--- analysis/visualization.py
+++ analysis/visualization.py
@@ -1,56 +1,36 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def plot_prices(df):
-    # Initialize the Seaborn plot
+def plot_prices(ax, df):
+    sns.lineplot(x=df.index, y='liquid_exchange_prices', data=df, label='Liquid Exchange Prices', ax=ax)
+    sns.lineplot(x=df.index, y='portfolio_prices', data=df, label='Portfolio Prices', ax=ax)
+    ax.set_title('Prices Over Time')
+    ax.set_xlabel('Index')
+    ax.set_ylabel('Price')
+    ax.legend()
+
+def plot_arbitrageur_relative_balances(ax, df):
+    sns.lineplot(x=df.index, y='arbitrageur_relative_balances_x', data=df, label='Relative Arbitrageur Balances X', ax=ax)
+    sns.lineplot(x=df.index, y='arbitrageur_relative_balances_y', data=df, label='Relative Arbitrageur Balances Y', ax=ax)
+    ax.set_title('Arbitrageur Balances Over Time')
+    ax.set_xlabel('Index')
+    ax.set_ylabel('Balance in wei')
+    ax.legend()
+
+def plot_portfolio_reserves(ax, df):
+    sns.lineplot(x=df.index, y='portfolio_reserves_x', data=df, label='Portfolio Reserves X', ax=ax)
+    sns.lineplot(x=df.index, y='portfolio_reserves_y', data=df, label='Portfolio Reserves Y', ax=ax)
+    ax.set_title('Portfolio Reserves Over Time')
+    ax.set_xlabel('Index')
+    ax.set_ylabel('Reserve in wei')
+    ax.legend()
+
+def plot_all(df, filename):
     sns.set_theme(style="darkgrid")
+    fig, axes = plt.subplots(3, 1, figsize=(10, 18))
 
-    # Plot multiple line plots
-    plt.figure(figsize=(10, 6))
-    sns.lineplot(x=df.index, y='liquid_exchange_prices', data=df, label='Liquid Exchange Prices')
-    sns.lineplot(x=df.index, y='portfolio_prices', data=df, label='Portfolio Prices')
+    plot_prices(axes[0], df)
+    plot_arbitrageur_relative_balances(axes[1], df)
+    plot_portfolio_reserves(axes[2], df)
 
-    # Add title and labels
-    plt.title('Prices Over Time')
-    plt.xlabel('Index')
-    plt.ylabel('Price')
-    plt.legend()
-
-    # Show the plot
-    plt.show()
-
-def plot_arbitrageur_relative_balances(df):
-    # Initialize the Seaborn plot
-    sns.set_theme(style="darkgrid")
-
-    # Plot multiple line plots
-    plt.figure(figsize=(10, 6))
-    sns.lineplot(x=df.index, y='arbitrageur_relative_balances_x', data=df, label='Relative Arbitrageur Balances X')
-    sns.lineplot(x=df.index, y='arbitrageur_relative_balances_y', data=df, label='Relative Arbitrageur Balances Y')
-
-    # Add title and labels
-    plt.title('Arbitrageur Balances Over Time')
-    plt.xlabel('Index')
-    plt.ylabel('Balance in wei')
-    plt.legend()
-
-    # Show the plot
-    plt.show()
-
-def plot_portfolio_reserves(df):
-    # Initialize the Seaborn plot
-    sns.set_theme(style="darkgrid")
-
-    # Plot multiple line plots
-    plt.figure(figsize=(10, 6))
-    sns.lineplot(x=df.index, y='portfolio_reserves_x', data=df, label='Portfolio Reserves X')
-    sns.lineplot(x=df.index, y='portfolio_reserves_y', data=df, label='Portfolio Reserves Y')
-
-    # Add title and labels
-    plt.title('Portfolio Reserves Over Time')
-    plt.xlabel('Index')
-    plt.ylabel('Reserve in wei')
-    plt.legend()
-
-    # Show the plot
-    plt.show()
+    plt.savefig(filename)
